c032447d1d26077f01df8614f737d2a37d78e754
Azir@9200 <aziruddin83@hotmail.com>
2023-09-10 00:59:21 +0100

    created files

---
+++ src/Components/Country/Country.jsx
@@ -0,0 +1,16 @@
+import './Country.css'
+const Country = ({ country }) => {
+  const { name, flags, population, area } = country;
+
+  return (
+    <div className='country'>
+      <h3>{name.common} </h3>
+      <img src={flags.png} alt="" />
+      <p> population: {population} </p>
+      <p>area: {area} </p>
+
+    </div>
+  );
+};
+
+export default Country;
